ec4e30d481bb487b73666e2b3f443d6bd28e32ea
amp <197376797+AmpElectrecuted@users.noreply.github.com>
2025-02-14 09:51:20 +0000

    chore: update APP_NAME to 'AmpMod'

--- src/lib/brand.js
+++ src/lib/brand.js
@@ -1,5 +1,5 @@
 // Legacy export format because this is used by some build-time scripts stuck in the past.
 // eslint-disable-next-line import/no-commonjs
 module.exports = {
-    APP_NAME: Math.random() < 0.5 ? "UltiBlocks" : "AmpMod"
+    APP_NAME: 'AmpMod'
 };
